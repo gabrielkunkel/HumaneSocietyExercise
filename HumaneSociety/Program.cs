a0d45e182ca4a2117fb6a48b9929add319e0c323
gabrielkunkel <henrysaltandson@gmail.com>
2019-10-22 14:01:22 -0500

    add getAnimalId, working updateAnimal

--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HumaneSociety
+﻿namespace HumaneSociety
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            PointOfEntry.Run();
-        }
+      PointOfEntry.Run();
     }
+  }
 }
